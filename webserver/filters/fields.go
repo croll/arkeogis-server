7d14035f931f9c7a2c829414ca8b8abf14ad70dd
Nicolas Dimitrijevic <nicolas@croll.fr>
2016-02-03 18:04:53 +0100

    add better error handling in fields sanitizers

--- webserver/filters/fields.go
+++ webserver/filters/fields.go
@@ -7,14 +7,21 @@ import (
 	"strconv"
 )
 
+type FieldError struct {
+	FieldPath   string `json:"field_path"`
+	FieldName   string `json:"field_name"`
+	ErrorString string `json:"error_string"`
+}
+
 // SanitizeStruct will sanitize all fields of a struct (o must be a pointer to this struct)
-func SanitizeStruct(o interface{}) []string {
-	errors := []string{}
-	sanitizeStruct(o, &errors)
+// return an array of string, one string per error
+func SanitizeStruct(o interface{}) []FieldError {
+	errors := []FieldError{}
+	sanitizeStruct(o, "", &errors)
 	return errors
 }
 
-func sanitizeStruct(o interface{}, errors *[]string) {
+func sanitizeStruct(o interface{}, path string, errors *[]FieldError) {
 	fmt.Println("sanitisz : ", o)
 	st := reflect.TypeOf(o)
 	vt := reflect.ValueOf(o)
@@ -27,12 +34,20 @@ func sanitizeStruct(o interface{}, errors *[]string) {
 	for i := 0; i < st.NumField(); i++ {
 		field := st.Field(i)
 		value := vt.Field(i)
-		fmt.Println("field", i, ":", field.Name)
-		if field.Type.Kind() == reflect.Struct {
-			fmt.Println("sub sanitize : ", field.Name)
-			sanitizeStruct(value.Interface(), errors)
+
+		n_path := ""
+		if path != "" {
+			n_path = path + "." + field.Name
 		} else {
-			sanitizeField(field, value, errors)
+			n_path = field.Name
+		}
+
+		fmt.Println("path", n_path)
+
+		if field.Type.Kind() == reflect.Struct {
+			sanitizeStruct(value.Interface(), n_path, errors)
+		} else {
+			sanitizeField(field, value, n_path, errors)
 		}
 	}
 }
@@ -50,9 +65,9 @@ func DefaultStruct(o interface{}) {
 	}
 }
 
-func sanitizeField(field reflect.StructField, value reflect.Value, errors *[]string) {
-	sanitizeFieldMin(field, value, errors)
-	sanitizeFieldMax(field, value, errors)
+func sanitizeField(field reflect.StructField, value reflect.Value, path string, errors *[]FieldError) {
+	sanitizeFieldMin(field, value, path, errors)
+	sanitizeFieldMax(field, value, path, errors)
 }
 
 // setFieldToDefault if there is a default value
@@ -77,17 +92,21 @@ func setFieldToDefault(field reflect.StructField, value reflect.Value) {
 	}
 }
 
-func setFieldError(field reflect.StructField, value reflect.Value, errors *[]string) {
+func setFieldError(field reflect.StructField, value reflect.Value, path string, errors *[]FieldError) {
 	log.Println("setting", field.Name, "to default", field.Tag.Get("default"))
 	setFieldToDefault(field, value)
 	s_error := field.Tag.Get("error")
 	if len(s_error) > 0 {
-		*errors = append(*errors, s_error)
+		*errors = append(*errors, FieldError{
+			FieldPath:   path,
+			FieldName:   field.Name,
+			ErrorString: s_error,
+		})
 	}
 }
 
 // sanitizeFieldMin check if field value is bellow minimum. If true, true is returned
-func sanitizeFieldMin(field reflect.StructField, value reflect.Value, errors *[]string) bool {
+func sanitizeFieldMin(field reflect.StructField, value reflect.Value, path string, errors *[]FieldError) bool {
 	s_min := field.Tag.Get("min")
 
 	if len(s_min) == 0 {
@@ -98,19 +117,19 @@ func sanitizeFieldMin(field reflect.StructField, value reflect.Value, errors *[]
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
 		min, _ := strconv.ParseInt(s_min, 10, 64)
 		if value.Int() < min {
-			setFieldError(field, value, errors)
+			setFieldError(field, value, path, errors)
 			return true
 		}
 	case reflect.Float32, reflect.Float64:
 		min, _ := strconv.ParseFloat(s_min, 64)
 		if value.Float() < min {
-			setFieldError(field, value, errors)
+			setFieldError(field, value, path, errors)
 			return true
 		}
 	case reflect.String:
 		min, _ := strconv.Atoi(s_min)
 		if len(value.String()) < min {
-			setFieldError(field, value, errors)
+			setFieldError(field, value, path, errors)
 			return true
 		}
 	default:
@@ -121,7 +140,7 @@ func sanitizeFieldMin(field reflect.StructField, value reflect.Value, errors *[]
 }
 
 // sanitizeFieldMax check if field value is above maxium. If true, true is returned
-func sanitizeFieldMax(field reflect.StructField, value reflect.Value, errors *[]string) bool {
+func sanitizeFieldMax(field reflect.StructField, value reflect.Value, path string, errors *[]FieldError) bool {
 	s_max := field.Tag.Get("max")
 
 	if len(s_max) == 0 {
@@ -132,19 +151,19 @@ func sanitizeFieldMax(field reflect.StructField, value reflect.Value, errors *[]
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
 		max, _ := strconv.ParseInt(s_max, 10, 64)
 		if value.Int() > max {
-			setFieldError(field, value, errors)
+			setFieldError(field, value, path, errors)
 			return true
 		}
 	case reflect.Float32, reflect.Float64:
 		max, _ := strconv.ParseFloat(s_max, 64)
 		if value.Float() > max {
-			setFieldError(field, value, errors)
+			setFieldError(field, value, path, errors)
 			return true
 		}
 	case reflect.String:
 		max, _ := strconv.Atoi(s_max)
 		if len(value.String()) > max {
-			setFieldError(field, value, errors)
+			setFieldError(field, value, path, errors)
 			return true
 		}
 	default:
