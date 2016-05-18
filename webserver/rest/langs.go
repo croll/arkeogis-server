32090bcb69ae273cd0261b21faeaaf78ce5e44be
Christophe Beveraggi <beve@croll.fr>
2016-05-18 17:08:32 +0200

    Lang REST API update

--- webserver/rest/langs.go
+++ webserver/rest/langs.go
@@ -25,10 +25,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"reflect"
 
 	db "github.com/croll/arkeogis-server/db"
 	routes "github.com/croll/arkeogis-server/webserver/routes"
 )
+
+// LangGetParams are params received by REST query
+type LangGetParams struct {
+	Active int
+}
 
 func init() {
 	Routes := []*routes.Route{
@@ -37,12 +43,17 @@ func init() {
 			Description: "Get languages list that are available",
 			Func:        LangList,
 			Method:      "GET",
+			Params:      reflect.TypeOf(LangGetParams{}),
+			Permissions: []string{},
 		},
 	}
 	routes.RegisterMultiple(Routes)
 }
 
+// LangList return the list of langs in arkeogis
 func LangList(w http.ResponseWriter, r *http.Request, proute routes.Proute) {
+
+	params := proute.Params.(*LangGetParams)
 
 	langs := []struct {
 		Id      uint32 `json:"id"`
@@ -50,8 +61,15 @@ func LangList(w http.ResponseWriter, r *http.Request, proute routes.Proute) {
 		Name    string `json:"name"`
 	}{}
 
-	err := db.DB.Select(&langs, "SELECT id, iso_code as isocode, name FROM lang LEFT JOIN lang_tr ON lang_tr.lang_id = lang.id AND lang_tr.lang_id_tr = $1 WHERE active = true AND iso_code != 'D'",
-		proute.Lang1.Id)
+	fmt.Println(params)
+
+	q := "SELECT id, iso_code as isocode, name FROM lang LEFT JOIN lang_tr ON lang_tr.lang_id = lang.id WHERE iso_code != 'D' AND lang_tr.lang_id_tr = $1"
+
+	if params.Active == 1 {
+		q += " AND active = true"
+	}
+
+	err := db.DB.Select(&langs, q, proute.Lang1.Id)
 	if err != nil {
 		fmt.Println("err: ", err)
 		return
