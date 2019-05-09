ada25a942aa03fd9a7101a2a6594c85efa4217ef
Nicolas Dimitrijevic <niclone@croll.fr>
2019-05-09 14:47:51 +0200

    update query to join license table, export it on omeka site / Dubline Core:Rights

--- export/omeka.go
+++ export/omeka.go
@@ -176,6 +176,7 @@ func SitesAsCSV(siteIDs []int, isoCode string, includeDbName bool, tx *sqlx.Tx) 
 		OwnerUser    model.User                 `json:"owneruser"`
 		Authors      []model.User               `json:"authors"`
 		Default_language_tr model.Lang_tr			`json:"default_language_tr"`
+		License		model.License				`json:"license"`
 	}
 
 
@@ -305,9 +306,14 @@ func SitesAsCSV(siteIDs []int, isoCode string, includeDbName bool, tx *sqlx.Tx) 
 		FROM (
 		  SELECT l.* FROM "lang_tr" "l" WHERE l.lang_isocode = db.Default_language AND l.lang_isocode_tr='fr'
 		) as items
-	  ) AS default_language_tr FROM "database" "db" WHERE db.id=284
+	  ) AS default_language_tr, 
+	  ( SELECT row_to_json(items)
+		FROM (
+		  SELECT license.* FROM "license" "license" WHERE license.id = db.license_id
+		) as items
+	  ) AS license FROM "database" "db" WHERE db.id=284
 	) as items
-			`
+	`
 
 	fmt.Println("query: "+q)
 	rows2, err := tx.Query(q)
@@ -452,7 +458,7 @@ func SitesAsCSV(siteIDs []int, isoCode string, includeDbName bool, tx *sqlx.Tx) 
 				// champs : Licence de la base
 				// type : individuel
 				// Licence de la base de données déclarée dans ArkeoGIS.
-				"",
+				database.License.Name,
 	
 				// URI Site
 				// champs : url fiche base de données
