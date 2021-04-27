412f4d79a8ff11fd7f4bb829c6150ff95d432012
Nicolas Dimitrijevic <niclone@croll.fr>
2021-04-27 14:37:26 +0200

    remove re_use import code, it is not translation aware

--- webserver/rest/import.go
+++ webserver/rest/import.go
@@ -395,7 +395,6 @@ type ImportStep3T struct {
 	Project_ID             int      `min:"1" error:"Project id is mandatory"`
 	Published              bool
 	Public                 bool
-	Re_use				   string
 	Description            []struct {
 		Lang_Isocode string
 		Text         string
@@ -416,7 +415,7 @@ func ImportStep3(w http.ResponseWriter, r *http.Request, proute routes.Proute) {
 	fmt.Println("DATABASE", d)
 	d.Get(tx)
 
-	err = d.UpdateFields(tx, params, "type", "declared_creation_date", "license_id", "scale_resolution", "state", "published", "public", "re_use")
+	err = d.UpdateFields(tx, params, "type", "declared_creation_date", "license_id", "scale_resolution", "state", "published", "public")
 
 	if err != nil {
 		log.Println("Error updating database fields: ", err)
