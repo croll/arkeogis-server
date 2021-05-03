5da45032a4aed85a65753495df49e5ec42aa22bb
Nicolas Dimitrijevic <niclone@croll.fr>
2021-05-03 10:49:35 +0200

    export xml interoperable wip

--- export/interoperable.go
+++ export/interoperable.go
@@ -26,7 +26,7 @@
 	"log"
 	//"math"
 	//"strconv"
-	//"strings"
+	"strings"
  	model "github.com/croll/arkeogis-server/model"
 	//"github.com/croll/arkeogis-server/translate"
 	"github.com/jmoiron/sqlx"
@@ -42,23 +42,46 @@
   xmlns:dcterms="http://purl.org/dc/terms/">
   */
 
-func InteroperableExportXml(tx *sqlx.Tx, w io.Writer, databaseId int, lang string) (err error) {
-	type StringL struct {
-		Content				string		`xml:",innerxml"`
-		Lang				string		`xml:"xml:lang,attr"`
-	}
-	type Metadata struct {
-		XMLName   			xml.Name 	`xml:"metadata"`
-		Xmlns				string   	`xml:"xmlns,attr"`
-		Xmlnsxsi			string   	`xml:"xmlns:xsi,attr"`
-		XsischemaLocation	string   	`xml:"xsi:schemaLocation,attr"`
-		Xmlnsdc				string		`xml:"xmlns:dc,attr"`
-		Xmlnsdcterms		string		`xml:"xmlns:dcterms,attr"`
+type StringL struct {
+	Content				string		`xml:",innerxml"`
+	Lang				string		`xml:"xml:lang,attr"`
+}
 
-		DcTitle			    string		`xml:"dc:title"`
-		DcCreator			[]string	`xml:"dc:creator"`
-		DcSubject			[]string	`xml:"dc:subject"`
+func readMappedToStringL(mapped map[string]string) []StringL {
+	var strings []StringL
+	for l, s := range mapped {
+		strings = append(strings, StringL{s, l})
+	}
+	return strings
+}
+
+type XsiTyped struct {
+	Content				string		`xml:",innerxml"`
+	Xsitype				string		`xml:"xsi:type,attr,omitempty"`
+}
+
+func InteroperableExportXml(tx *sqlx.Tx, w io.Writer, databaseId int, lang string) (err error) {
+	type Metadata struct {
+		XMLName   			xml.Name 		`xml:"metadata"`
+		Xmlns				string   		`xml:"xmlns,attr"`
+		Xmlnsxsi			string   		`xml:"xmlns:xsi,attr"`
+		XsischemaLocation	string   		`xml:"xsi:schemaLocation,attr"`
+		Xmlnsdc				string			`xml:"xmlns:dc,attr"`
+		Xmlnsdcterms		string			`xml:"xmlns:dcterms,attr"`
+
+		DcTitle			    string			`xml:"dc:title"`
+		DcCreator			[]string		`xml:"dc:creator"`
+		DcSubject			[]StringL		`xml:"dc:subject"`
 		DcDescription		[]StringL		`xml:"dc:description"`
+		DcPublishers		[]XsiTyped	    `xml:"dc:publisher"`
+		DcContributors      []string		`xml:"dc:contributor"`
+		DcDate              XsiTyped        `xml:"dc:date"`
+		DctermsIssued       XsiTyped        `xml:"dcterms:issued"`
+		DctermsModified     XsiTyped        `xml:"dcterms:modified"`
+		DcType			    XsiTyped		`xml:"dc:type"`   // @TODO: check if this is ok
+		DcFormat			string			`xml:"dc:format"` // @TODO: check if this is ok
+		DcIdentifier        []XsiTyped		`xml:"dc:identifier"`
+		DcBibliographicCitation			    []StringL		`xml:"dc:bibliographicCitation"`
 		//Dc			    string		`xml:"dc:"`
 	}
 
@@ -81,6 +104,34 @@ func InteroperableExportXml(tx *sqlx.Tx, w io.Writer, databaseId int, lang strin
 	v.Xmlnsdcterms = "http://purl.org/dc/terms/"
 
 	v.DcTitle = dbInfos.Name
+	v.DcCreator = dbInfos.GetAuthorsStrings()
+	v.DcSubject = readMappedToStringL(dbInfos.Subject)
+	v.DcDescription = readMappedToStringL(dbInfos.Description)
+	v.DcPublishers = []XsiTyped{
+		XsiTyped{dbInfos.Editor, ""},
+		XsiTyped{dbInfos.Editor_url, "dcterms:URI"},
+	}
+	v.DcContributors = strings.Split(dbInfos.Contributor, ", ")
+	v.DcDate = XsiTyped{dbInfos.Declared_creation_date.Format("2006-01-02"), "dcterms:W3CDTF"}
+	v.DctermsIssued = XsiTyped{dbInfos.Created_at.Format("2006-01-02"), "dcterms:W3CDTF"}
+	v.DctermsModified = XsiTyped{dbInfos.Updated_at.Format("2006-01-02"), "dcterms:W3CDTF"}
+	v.DcType = XsiTyped{"dataset", "dcterms:DCMIType"}
+	v.DcFormat = "text/csv"
+
+	if len(dbInfos.Handles) > 0 {
+		v.DcIdentifier = []XsiTyped{
+			XsiTyped{dbInfos.Handles[0], "dcterms:URI"}
+		}
+	}
+
+	v.DcBibliographicCitation = readMappedToStringL(dbInfos.Bibliography)
+	
+
+
+	//v.DcCreator = strings.Split(dbInfos.Editor, ",")
+
+	w.Write([]byte(`<?xml version="1.0" encoding="UTF-8"?>`+"\n"))
+	w.Write([]byte(`<?xml-stylesheet href="https://arkeogis.org/css/dataset-dublin-core.xsl" type="text/xsl"?>`+"\n"))
 
 	enc := xml.NewEncoder(w)
 	enc.Indent("  ", "    ")
